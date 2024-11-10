f0c101b2607c4fc89479a81e82a78264a5b0ec21
edo1z <endo.yuta@logicky.com>
2024-11-10 13:41:41 +0900

    コレクションにデータ格納

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-import weaviate, { vectorizer, generative } from 'weaviate-client';
+import weaviate from 'weaviate-client';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const wcdUrl = process.env.WEAVIATE_URL;
 const wcdApiKey = process.env.WEAVIATE_API_KEY;
+const openAIKey = process.env.OPENAI_API_KEY;
 
 const client = await weaviate.connectToWeaviateCloud(
   wcdUrl,
   {
     authCredentials: new weaviate.ApiKey(wcdApiKey),
+    headers: {
+      'X-OpenAI-Api-Key': openAIKey,
+    },
   }
 );
 
-await client.collections.create({
-  name: 'Question',
-  vectorizers: vectorizer.text2VecOpenAI(),
-  generative: generative.openAI(),
-});
+// データ取得関数
+async function getJsonData() {
+  const file = await fetch(
+    'https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json'
+  );
+  return file.json();
+}
+
+// データインポート関数
+async function importQuestions() {
+  const questions = client.collections.get('Question');
+  const data = await getJsonData();
+  const result = await questions.data.insertMany(data);
+  console.log('Insertion response: ', result);
+}
+
+await importQuestions();
 
 client.close();
